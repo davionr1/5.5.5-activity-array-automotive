d9e60b4fdf5dd4a37eea97dda09265cbcc900db4
Davion Roachford <davionr1@icloud.com>
2022-09-30 05:26:47 -0700

    inheritance practice

--- mecurySedan.js
+++ mecurySedan.js
@@ -1,3 +1,49 @@
+import {Vehicle} from './vehicle.js'
+class Car extends Vehicle {
+constructor(make, model, year, color, mileage) {
+    super (make, model, year, color, mileage) 
+    this.maximumPassengers = 5
+    this.passenger = 0
+    this.numberOfWheels = 4
+    this.maximumPassengers = 160
+    this.fuel = 10
+    this.scheduleService = false
+}
+
+
+loadPassenger(num) {
+    if (this.passenger < this.maxPassengers) {
+        if ((num + this.passenger) <= this.maxPassengers) {
+            this.passenger = num;
+            return this.passenger;               
+        } 
+        else {
+            console.log(this.model + " " + this.make + " theres not have enough space to take all passengers.");
+
+        }
+    } 
+    else {
+        console.log(this.model + " " + this.make + "is full");
+    }
+}
+
+start() {
+    if (this.fuel > 0) {
+        return this.started = true;
+        console.log("engine started");
+    } else {
+        return this.started = false;
+        console.log("engine cannot start");
+    }
+}
+
+scheduleService(mileage){
+    if(this.mileage > 30000){
+        this.scheduleService = true
+        return this.scheduleService
+    }
+}
+}
 //this includes the vehicle class as a module
 const VehicleModule = require("./vehicleBaseClass")
 
